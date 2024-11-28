ffe1824d1166bb0408675eff5777caf5d90b00a4
DanielVazquez <daniel.vazquez@iqs.url.edu>
2024-11-28 09:41:11 +0100

    Initial commit

---
+++ cpfuncs.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+def FOPDTprocess(y,t,u, d,Kp, taup, thetap, Kd, Input_start):
+    if t<(thetap+Input_start):
+        dydt = 0.0  # time delay
+    else:
+        dydt = (1.0/taup) * (-y + Kp * u + Kd*d)
+    return dydt
+
+
+def pidPlot(Kp, taup, thetap, Kd, Kc,tauI,tauD, process, tf, Input_start, 
+            Input_change, n, Input_change_type = "step", DerivateKickFix = False,
+            sp_ramp_time = 5, type_of_process = "servo", controller_type = "PID"):
+    size_temp_point = n/tf
+    t = np.linspace(0,tf,n) # create time vector
+    P= np.zeros(n)          # initialize proportional term
+    I = np.zeros(n)         # initialize integral term
+    D = np.zeros(n)         # initialize derivative term
+    e = np.zeros(n)         # initialize error
+    OP = np.zeros(n)        # initialize controller output
+    PV = np.zeros(n)        # initialize process variable
+    SP = np.zeros(n)        # initialize setpoint
+    Input_step = int(Input_start/(tf/(n-1))+1) # setpoint start
+    d = np.zeros(n)         # initialize disturbance
+    if type_of_process == "servo":
+        if Input_change_type == "step":
+            SP[0:Input_step] = 0.0     # define setpoint
+            SP[Input_step:n] = Input_change     # step up
+        elif Input_change_type == "ramp":
+            SP[0:Input_step] = 0.0
+            SP[Input_step:Input_step+int(sp_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(sp_ramp_time*size_temp_point))
+            SP[Input_step+int(sp_ramp_time*size_temp_point):n] = Input_change
+    elif type_of_process == "regulator":
+        if Input_change_type == "step":
+            d[0:Input_step] = 0.0     # define setpoint
+            d[Input_step:n] = Input_change
+        elif Input_change_type == "ramp":
+            d[0:Input_step] = 0.0
+            d[Input_step:Input_step+int(sp_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(sp_ramp_time*size_temp_point))
+            d[Input_step+int(sp_ramp_time*size_temp_point):n] = Input_change
+    y0 = 0.0                # initial condition
+    # loop through all time steps
+    for i in range(1,n):
+        # simulate process for one time step
+        ts = [t[i-1],t[i]]         # time interval
+        y = odeint(process,y0,ts,args=(OP[i-1],d[i-1], Kp, taup, thetap, Kd, Input_start))  # compute next step
+        y0 = y[1]                  # record new initial condition
+        # calculate new OP with PID
+        PV[i] = y[1]               # record PV
+        e[i] = SP[i] - PV[i]       # calculate error = SP - PV
+        dt = t[i] - t[i-1]         # calculate time step
+        P[i] = Kc * e[i]           # calculate proportional term
+        if tauI != 0:
+            I[i] = I[i-1] + (Kc/tauI) * e[i] * dt  # calculate integral term
+        else:
+            I[i] = 0
+        if DerivateKickFix:
+            D[i] = -Kc * tauD * (PV[i]-PV[i-1])/dt # calculate derivative term
+        else:
+            D[i] = +Kc * tauD * (e[i]-e[i-1])/dt
+
+        if controller_type == "PI":
+            D[i] = 0
+        elif controller_type == "P":
+            I[i] = 0
+            D[i] = 0
+        elif controller_type == "I":
+            P[i] = 0
+            D[i] = 0
+        
+        OP[i] = P[i] + I[i] + D[i] # calculate new controller output
+        
+    # plot PID response
+    plt.figure(1,figsize=(15,7))
+    plt.subplot(2,2,1)
+    plt.plot(t,SP,'k-',linewidth=2,label='Setpoint (SP)')
+    plt.plot(t,PV,'r:',linewidth=2,label='Process Variable (PV)')
+    plt.legend(loc='best')
+    plt.xlabel('time')
+    plt.grid(alpha = 0.2)
+    plt.subplot(2,2,2)
+    plt.plot(t,P,'g.-',linewidth=2,label=r'Proportional = $K_c \; e(t)$')
+    plt.plot(t,I,'b-',linewidth=2,label=r'Integral = $\frac{K_c}{\tau_I} \int_{i=0}^{n_t} e(t) \; dt $')
+    plt.plot(t,D,'r--',linewidth=2,label=r'Derivative = $-K_c \tau_D \frac{d(PV)}{dt}$' if DerivateKickFix else r'Derivative = $K_c \tau_D \frac{d(e)}{dt}$')    
+    plt.legend(loc='best')
+    plt.xlabel('time')
+    plt.grid(alpha = 0.2)
+    plt.subplot(2,2,3)
+    plt.plot(t,e,'m--',linewidth=2,label='Error (e=SP-PV)')
+    plt.legend(loc='best')
+    plt.xlabel('time')
+    plt.grid(alpha = 0.2)
+    plt.subplot(2,2,4)
+    plt.plot(t,OP,'b--',linewidth=2,label='Controller Output (OP)')
+    plt.legend(loc='best')
+    plt.xlabel('time')
+    plt.grid(alpha = 0.2)
+    plt.tight_layout()
+    ax = plt.gcf()
+    return PV.max(), ax
+
+
+if __name__=="__main__":
+    dictionary_options = {"Kp":4,
+                          "taup":1,
+                          "thetap":3,
+                          "Kc":1,
+                          "tauI":1,
+                          "tauD":00,
+                          "process":FOPDTprocess,
+                          "tf":30,
+                          "Input_start":1,
+                          "Input_change":1,
+                          "n":10000,
+                          "Input_change_type":"ramp",
+                          "DerivateKickFix":True,
+                          "type_of_process":"regulator",
+                          "Kd":1,
+                          "controller_type":"PID"}
+    PID = pidPlot(**dictionary_options)
+    print("Maximum overshoot: ", PID)
---
+++ streamlit_app.py
@@ -0,0 +1,74 @@
+from cpfuncs import pidPlot, FOPDTprocess
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(
+    page_title="Feedback control playground",
+    page_icon="🎚️",
+    layout="wide",
+) 
+
+
+with st.expander("🗒️ About", expanded=False):
+    st.write("""
+    This is a web app to observe how different parameters affect a first
+    order model with dead time. The model is simulated using a PID controller. 
+    """)
+
+st.markdown("# Process & Controller information")
+st.markdown("The model of first-order plus dead time is:")
+st.markdown("#### $$\\tau_p \\frac{dy(t)}{dt} + y(t) = K_p·c(t) + K_d·d(t)$$")
+st.markdown('**Parameters of the process**  \nProcess gain $K_p$, process time constant $\\tau_p$ and process dead time $\\theta_p$. $K_d$ refers to the gain of the disturbances  \n  \n**Parameters of the controller**  \nController gain $K_c$, integral time constant $\\tau_I$ and derivative time constant $\\tau_D$.')
+controller_type = st.selectbox("Controller type", ["P", "I", "PI", "PID"])
+col1,col2,col3,col4, col5, col6 = st.columns(6)
+with col1:
+    Kp = st.slider("$K_p$", 0.0, 10.0)
+    Kc = st.slider("$K_c$", 0.0, 10.0)
+with col2:
+    taup = st.slider("$\\tau_p$", 0.1, 10.0)
+    tauI = st.slider("$\\tau_I$", 0.0, 10.0)
+with col3:
+    thetap = st.slider("$\\theta_p$", 0.0, 10.0)
+    tauD = st.slider("$\\tau_D$", 0.0, 10.0)
+with col4:
+    Kd = st.slider("$K_d$", 0.0, 10.0)
+    
+DerivativeFix = st.checkbox("Derivative kick fix")
+
+st.markdown("# Simulation data")
+st.markdown("Simulation time and input change data. In the case of servo problem, 'input' refers to the set point. In the case of the regulator problem, 'input' refers to the disturbance.")
+col1,col2,col3, _, _, _ = st.columns(6)
+with col1:
+    tf = st.slider("Simulation time", 1, 100, )
+with col2:
+    Input_start = st.slider("Time of the input change", 0.0, float(tf))
+with col3:
+    Input_change = st.slider("Change of the input", 0.0, 10.0)
+col1, col2 = st.columns(2)
+with col1:
+    Input_change_type = st.selectbox("Setpoint change type", ["step", "ramp"])
+with col2:
+    type_of_process = st.selectbox("Type of process", ["servo", "regulator"])    
+
+
+n = max(1000,tf*100) # Simulation time points
+
+
+dictionary_options = {"Kp":Kp,
+                      "taup":taup,
+                      "thetap":thetap,
+                      "Kc":Kc,
+                      "tauI":tauI,
+                      "tauD":tauD,
+                      "process":FOPDTprocess,
+                      "tf":tf,
+                      "Input_start":Input_start,
+                      "Input_change":Input_change,
+                      "n":n,
+                      "Input_change_type":Input_change_type,
+                      "DerivateKickFix":DerivativeFix,
+                      "Kd":Kd,
+                      "type_of_process":type_of_process,
+                      "controller_type":controller_type}
+
+st.pyplot(pidPlot(**dictionary_options)[1])
