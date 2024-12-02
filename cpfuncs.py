b0a1b41135a19addf23a7fe3a7e9d9ca8e21bc6f
DanielVazquez <daniel.vazquez@iqs.url.edu>
2024-12-02 17:26:49 +0100

    Added SOPDT model

--- cpfuncs.py
+++ cpfuncs.py
@@ -10,10 +10,17 @@ def FOPDTprocess(y,t,u, d,Kp, taup, thetap, Kd, Input_start):
         dydt = (1.0/taup) * (-y + Kp * u + Kd*d)
     return dydt
 
+def SOPDTprocess(y,t, u, d, Kp, zeta, taus, thetap, Kd, Input_start):
+    if t<(thetap+Input_start):
+        dydt = 0.0  # time delay
+        dy2dt2 = 0.0
+    else:
+        y, dydt = y
+        dy2dt2 = (-y + Kp * u + Kd*d - 2*zeta*taus*dydt)/taus**2
+    return [dydt, dy2dt2]
 
-def pidPlot(Kp, taup, thetap, Kd, Kc,tauI,tauD, process, tf, Input_start, 
-            Input_change, n, Input_change_type = "step", DerivateKickFix = False,
-            sp_ramp_time = 5, type_of_process = "servo", controller_type = "PID"):
+def calc_FOPDT_closed_loop(Kp, taup, thetap, Kd, Kc, tauI, tauD, tf, Input_start, Input_change, n, Input_change_type,
+                           DerivateKickFix, input_ramp_time = 5, type_of_process = "servo", controller_type = "PID"):
     size_temp_point = n/tf
     t = np.linspace(0,tf,n) # create time vector
     P= np.zeros(n)          # initialize proportional term
@@ -23,7 +30,7 @@ def pidPlot(Kp, taup, thetap, Kd, Kc,tauI,tauD, process, tf, Input_start,
     OP = np.zeros(n)        # initialize controller output
     PV = np.zeros(n)        # initialize process variable
     SP = np.zeros(n)        # initialize setpoint
-    Input_step = int(Input_start/(tf/(n-1))+1) # setpoint start
+    Input_step = int(Input_start/(tf/(n-1))+1) # Input start
     d = np.zeros(n)         # initialize disturbance
     if type_of_process == "servo":
         if Input_change_type == "step":
@@ -31,22 +38,23 @@ def pidPlot(Kp, taup, thetap, Kd, Kc,tauI,tauD, process, tf, Input_start,
             SP[Input_step:n] = Input_change     # step up
         elif Input_change_type == "ramp":
             SP[0:Input_step] = 0.0
-            SP[Input_step:Input_step+int(sp_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(sp_ramp_time*size_temp_point))
-            SP[Input_step+int(sp_ramp_time*size_temp_point):n] = Input_change
+            SP[Input_step:Input_step+int(input_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(input_ramp_time*size_temp_point))
+            SP[Input_step+int(input_ramp_time*size_temp_point):n] = Input_change
     elif type_of_process == "regulator":
         if Input_change_type == "step":
-            d[0:Input_step] = 0.0     # define setpoint
+            d[0:Input_step] = 0.0     # define disturbance
             d[Input_step:n] = Input_change
         elif Input_change_type == "ramp":
             d[0:Input_step] = 0.0
-            d[Input_step:Input_step+int(sp_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(sp_ramp_time*size_temp_point))
-            d[Input_step+int(sp_ramp_time*size_temp_point):n] = Input_change
+            d[Input_step:Input_step+int(input_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(input_ramp_time*size_temp_point))
+            d[Input_step+int(input_ramp_time*size_temp_point):n] = Input_change
+    
     y0 = 0.0                # initial condition
     # loop through all time steps
     for i in range(1,n):
         # simulate process for one time step
         ts = [t[i-1],t[i]]         # time interval
-        y = odeint(process,y0,ts,args=(OP[i-1],d[i-1], Kp, taup, thetap, Kd, Input_start))  # compute next step
+        y = odeint(FOPDTprocess,y0,ts,args=(OP[i-1],d[i-1], Kp, taup, thetap, Kd, Input_start))  # compute next step
         y0 = y[1]                  # record new initial condition
         # calculate new OP with PID
         PV[i] = y[1]               # record PV
@@ -61,18 +69,84 @@ def pidPlot(Kp, taup, thetap, Kd, Kc,tauI,tauD, process, tf, Input_start,
             D[i] = -Kc * tauD * (PV[i]-PV[i-1])/dt # calculate derivative term
         else:
             D[i] = +Kc * tauD * (e[i]-e[i-1])/dt
-
-        if controller_type == "PI":
-            D[i] = 0
-        elif controller_type == "P":
-            I[i] = 0
-            D[i] = 0
-        elif controller_type == "I":
+        
+        if "P" not in controller_type:
             P[i] = 0
+        if "I" not in controller_type:
+            I[i] = 0
+        if "D" not in controller_type:
             D[i] = 0
-        
+
         OP[i] = P[i] + I[i] + D[i] # calculate new controller output
+    return t, SP, PV, OP, P, I, D, e, DerivateKickFix
+
+
+def calc_SOPDT_closed_loop(Kp, taus, thetap, zeta, Kd, Kc, tauI, tauD, tf, Input_start, Input_change, n, Input_change_type,
+                           DerivateKickFix, input_ramp_time = 5, type_of_process = "servo", controller_type = "PID"):
+
+    size_temp_point = n/tf
+    t = np.linspace(0,tf,n) # create time vector
+    P= np.zeros(n)          # initialize proportional term
+    I = np.zeros(n)         # initialize integral term
+    D = np.zeros(n)         # initialize derivative term
+    e = np.zeros(n)         # initialize error
+    OP = np.zeros(n)        # initialize controller output
+    PV = np.zeros((n,2))    # initialize process variable. There are two because of derivative, so we will consider only the first column vector
+    SP = np.zeros(n)        # initialize setpoint
+    Input_step = int(Input_start/(tf/(n-1))+1) # Input start
+    d = np.zeros(n)         # initialize disturbance
+
+    
+    if type_of_process == "servo":
+        if Input_change_type == "step":
+            SP[0:Input_step] = 0.0     # define setpoint
+            SP[Input_step:n] = Input_change     # step up
+        elif Input_change_type == "ramp":
+            SP[0:Input_step] = 0.0
+            SP[Input_step:Input_step+int(input_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(input_ramp_time*size_temp_point))
+            SP[Input_step+int(input_ramp_time*size_temp_point):n] = Input_change
+    elif type_of_process == "regulator":
+        if Input_change_type == "step":
+            d[0:Input_step] = 0.0     # define disturbance
+            d[Input_step:n] = Input_change
+        elif Input_change_type == "ramp":
+            d[0:Input_step] = 0.0
+            d[Input_step:Input_step+int(input_ramp_time*size_temp_point)] = np.linspace(0,Input_change,int(input_ramp_time*size_temp_point))
+            d[Input_step+int(input_ramp_time*size_temp_point):n] = Input_change
+
+    y0 = [0.0, 0.0]         # initial condition PV, dPV/dt
+    # loop through all time steps
+    for i in range(1,n):
+        # simulate process for one time step
+        ts = [t[i-1],t[i]]         # time interval
+        y = odeint(SOPDTprocess,y0,ts,args=(OP[i-1],d[i-1], Kp, zeta, 
+                                            taus, thetap, Kd, Input_start))  # compute next step
+        y0 = y[1,:]                  # record new initial condition
+        # calculate new OP with PID
+        PV[i,:] = y[1,:]           # record PV
+        e[i] = SP[i] - PV[i,0]     # calculate error = SP - PV
+        dt = t[i] - t[i-1]         # calculate time step
+        P[i] = Kc * e[i]           # calculate proportional term
+        if tauI != 0:
+            I[i] = I[i-1] + (Kc/tauI) * e[i] * dt  # calculate integral term
+        else:
+            I[i] = 0
+        if DerivateKickFix:
+            D[i] = -Kc * tauD * (PV[i,0]-PV[i-1,0])/dt # calculate derivative term
+        else:
+            D[i] = +Kc * tauD * (e[i]-e[i-1])/dt
         
+        if "P" not in controller_type:
+            P[i] = 0
+        if "I" not in controller_type:
+            I[i] = 0
+        if "D" not in controller_type:
+            D[i] = 0
+
+        OP[i] = P[i] + I[i] + D[i] # calculate new controller output
+    return t, SP, PV, OP, P, I, D, e, DerivateKickFix
+
+def pidPlot(t, SP, PV, OP, P, I, D, e, DerivateKickFix):
     # plot PID response
     plt.figure(1,figsize=(15,7))
     plt.subplot(2,2,1)
@@ -122,13 +196,12 @@ def plot_Laplace_inverse(num, den):
 
 
 if __name__=="__main__":
-    dictionary_options = {"Kp":4,
+    FOPDT_dictionary_options = {"Kp":4,
                           "taup":1,
                           "thetap":3,
                           "Kc":1,
                           "tauI":1,
                           "tauD":00,
-                          "process":FOPDTprocess,
                           "tf":30,
                           "Input_start":1,
                           "Input_change":1,
@@ -138,5 +211,30 @@ if __name__=="__main__":
                           "type_of_process":"regulator",
                           "Kd":1,
                           "controller_type":"PID"}
-    PID = pidPlot(**dictionary_options)
+    
+    SOPDT_dictionary_options = {
+        "Kp":4,
+        "taus":1,
+        "thetap":3,
+        "zeta":0.5,
+        "Kd":1,
+        "Kc":1,
+        "tauI":2,
+        "tauD":0.50,
+        "tf":30,
+        "Input_start":1,
+        "Input_change":1,
+        "n":10000,
+        "Input_change_type":"step",
+        "DerivateKickFix":True,
+        "type_of_process":"servo",
+        "controller_type":"PID"
+    }
+    t, SP, PV, OP, P, I, D, e, DerivateKickFix = calc_FOPDT_closed_loop(**FOPDT_dictionary_options)
+    PID = pidPlot(t, SP, PV, OP, P, I, D, e, DerivateKickFix)
+    plt.show(PID[1])
+
+    t, SP, PV, OP, P, I, D, e, DerivateKickFix = calc_SOPDT_closed_loop(**SOPDT_dictionary_options)
+    PID_SO = pidPlot(t, SP, PV[:,0], OP, P, I, D, e, DerivateKickFix)
+    plt.show(PID_SO[1])
     print("Maximum overshoot: ", PID)
--- pages/2_🎚️_PID_Control.py
+++ pages/2_🎚️_PID_Control.py
@@ -1,4 +1,4 @@
-from cpfuncs import pidPlot, FOPDTprocess
+from cpfuncs import pidPlot, FOPDTprocess, calc_FOPDT_closed_loop, calc_SOPDT_closed_loop
 import streamlit as st
 
 st.set_page_config(
@@ -15,22 +15,36 @@ with st.expander("🗒️ About", expanded=False):
     """)
 
 st.markdown("# Process & Controller information")
-st.markdown("The first-order plus dead time model is:")
-st.markdown("#### $$\\tau_p \\frac{dy(t)}{dt} + y(t) = K_p·c(t) + K_d·d(t)$$")
-st.markdown('**Parameters of the process**  \nProcess gain $K_p$, process time constant $\\tau_p$ and process dead time $\\theta_p$. $K_d$ refers to the gain of the disturbances  \n  \n**Parameters of the controller**  \nController gain $K_c$, integral time constant $\\tau_I$ and derivative time constant $\\tau_D$.')
-controller_type = st.selectbox("Controller type", ["P", "I", "PI", "PID"])
+process_order = st.selectbox("Process order", ["FOPDT", "SOPDT"])
+if process_order == "FOPDT":
+    st.markdown("The first-order plus dead time model is:")
+    st.markdown("#### $$\\tau_p \\frac{dy(t)}{dt} + y(t) = K_p·c(t-\\theta_p) + K_d·d(t-\\theta_p)$$")
+    st.markdown('**Parameters of the process**  \nProcess gain $K_p$, process time constant $\\tau_p$ and process dead time $\\theta_p$. $K_d$ refers to the gain of the disturbances  \n  \n**Parameters of the controller**  \nController gain $K_c$, integral time constant $\\tau_I$ and derivative time constant $\\tau_D$.')
+elif process_order == "SOPDT":
+    st.markdown("The second-order plus dead time model is:")
+    st.markdown("#### $$\\tau_s^2 \\frac{d^2y(t)}{dt^2} + 2\\zeta\\tau_s\\frac{dy(t)}{dt} + y(t) = K_p·c(t-\\theta_p) + K_d·d(t-\\theta_p)$$")
+    st.markdown('**Parameters of the process**  \nProcess gain $K_p$, second order time constant $\\tau_s$, damping ratio $\\zeta$ and process dead time $\\theta_p$. $K_d$ refers to the gain of the disturbances  \n  \n**Parameters of the controller**  \nController gain $K_c$, integral time constant $\\tau_I$ and derivative time constant $\\tau_D$.')
+
+controller_type = st.selectbox("Controller type", ["P", "I", "D", "PI", "PD", "ID", "PID"])
 col1,col2,col3,col4, col5, col6 = st.columns(6)
 with col1:
     Kp = st.slider("$K_p$", 0.0, 10.0, value = 3.0)
     Kc = st.slider("$K_c$", 0.0, 10.0, value = 0.0)
 with col2:
-    taup = st.slider("$\\tau_p$", 0.1, 10.0, value = 2.0)
+    if process_order == "FOPDT":
+        taup = st.slider("$\\tau_p$", 0.1, 10.0, value = 2.0)
+        taus = 0
+    elif process_order == "SOPDT":
+        taus = st.slider("$\\tau_s$", 0.1, 10.0, value = 2.0)
+        taup = 0
     tauI = st.slider("$\\tau_I$", 0.0, 10.0)
 with col3:
     thetap = st.slider("$\\theta_p$", 0.0, 10.0)
-    tauD = st.slider("$\\tau_D$", 0.0, 10.0)
+    tauD = st.slider("$\\tau_D$", 0.0, 2.0)
 with col4:
     Kd = st.slider("$K_d$", 0.0, 10.0, value = 1.0)
+    if process_order == "SOPDT":
+        zeta = st.slider("$\\zeta$", 0.0, 10.0, value = 0.5)
     
 DerivativeFix = st.checkbox("Derivative kick fix")
 
@@ -52,22 +66,42 @@ with col2:
 
 n = max(1000,tf*100) # Simulation time points
 
+if process_order == "FOPDT":
+    dictionary_options = {"Kp":Kp,
+                        "taup":taup,
+                        "thetap":thetap,
+                        "Kc":Kc,
+                        "tauI":tauI,
+                        "tauD":tauD,
+                        "tf":tf,
+                        "Input_start":Input_start,
+                        "Input_change":Input_change,
+                        "n":n,
+                        "Input_change_type":Input_change_type,
+                        "DerivateKickFix":DerivativeFix,
+                        "Kd":Kd,
+                        "type_of_process":type_of_process,
+                        "controller_type":controller_type}
 
-dictionary_options = {"Kp":Kp,
-                      "taup":taup,
-                      "thetap":thetap,
-                      "Kc":Kc,
-                      "tauI":tauI,
-                      "tauD":tauD,
-                      "process":FOPDTprocess,
-                      "tf":tf,
-                      "Input_start":Input_start,
-                      "Input_change":Input_change,
-                      "n":n,
-                      "Input_change_type":Input_change_type,
-                      "DerivateKickFix":DerivativeFix,
-                      "Kd":Kd,
-                      "type_of_process":type_of_process,
-                      "controller_type":controller_type}
+    t, SP, PV, OP, P, I, D, e, DerivateKickFix = calc_FOPDT_closed_loop(**dictionary_options)
+    st.pyplot(pidPlot(t, SP, PV, OP, P, I, D, e, DerivateKickFix)[1])
 
-st.pyplot(pidPlot(**dictionary_options)[1])
+elif process_order == "SOPDT":
+    dictionary_options = {"Kp":Kp,
+                        "taus":taus,
+                        "thetap":thetap,
+                        "Kc":Kc,
+                        "tauI":tauI,
+                        "tauD":tauD,
+                        "tf":tf,
+                        "Input_start":Input_start,
+                        "Input_change":Input_change,
+                        "n":n,
+                        "Input_change_type":Input_change_type,
+                        "DerivateKickFix":DerivativeFix,
+                        "Kd":Kd,
+                        "zeta":zeta,
+                        "type_of_process":type_of_process,
+                        "controller_type":controller_type}
+    t, SP, PV, OP, P, I, D, e, DerivateKickFix = calc_SOPDT_closed_loop(**dictionary_options)
+    st.pyplot(pidPlot(t, SP, PV[:,0], OP, P, I, D, e, DerivateKickFix)[1])
